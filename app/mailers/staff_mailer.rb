862d9106e22da2c004b2998a9ad3ce9d9005494c
Anna Kryczka <aniakryczka@gmail.com>
2016-02-15 00:08:17 +0000

    Fixed emails

--- app/mailers/staff_mailer.rb
+++ app/mailers/staff_mailer.rb
@@ -5,7 +5,7 @@ class StaffMailer < ApplicationMailer
     mail to: staff.email, subject: "WVBR Account activation"
   end
 
-  def password_reset
+  def password_reset(staff)
     @staff = staff
     mail to: staff.email, subject: "WVBR Password Reset"
   end
