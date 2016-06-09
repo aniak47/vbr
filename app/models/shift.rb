4b21ce06228a4728b5bbbf5e41c9d9a5b078fe48
Anna Kryczka <aniakryczka@gmail.com>
2016-06-09 01:38:03 +0000

    concert log

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -5,5 +5,5 @@ class Shift < ActiveRecord::Base
     validates :day, presence: true
     validates :start, presence: true
     validates :end, presence: true
-    validates :blurb, presence: true, length: { maximum: 200 }
+    validates :blurb, length: { maximum: 200 }
 end
