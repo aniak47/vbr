90aefc9b4039dde4a9428f046434c962c640fa11
Anna Kryczka <aniakryczka@gmail.com>
2016-06-16 15:21:59 +0000

    fixed mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'ancient-spire-6970.herokuapp.com'
+  host = 'wvbrfm.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
