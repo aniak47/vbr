9abd9b8ad33ac29a0c4906aff9bcce43f5482d46
Anna Kryczka <aniakryczka@gmail.com>
2016-10-09 22:39:21 +0000

    PalPay button

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ActiveRecord::Base
     
     validates :name, presence: true, length: { maximum: 100 }
     validates :date, presence: true
+    validates :time, presence: true
     validates :catergory, inclusion: { in: %w(Promo Concert Other)}, allow_blank: true
     
     default_scope -> { order(:date) }
