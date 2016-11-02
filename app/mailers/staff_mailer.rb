5b25f4d9ea2ca06469f6ee38a9114578ac92317e
Anna Kryczka <aniakryczka@gmail.com>
2016-11-02 21:32:30 +0000

    Altered mailer

--- app/mailers/staff_mailer.rb
+++ app/mailers/staff_mailer.rb
@@ -3,6 +3,7 @@ class StaffMailer < ApplicationMailer
   def account_activation(staff)
     @staff = staff
     mail to: "web@wvbr.com", subject: "WVBR Account Created"
+    mail to: staff.email, subject: "WVBR Account Created"
   end
 
   def password_reset(staff)
