2c68ae111eb06b6de34a0fe7d1e56cf1a060a7dd
Anna Kryczka <aniakryczka@gmail.com>
2017-01-12 03:11:26 +0000

    Added school closing info

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,9 +3,17 @@ class StoreController < ApplicationController
   require 'net/http'
   
   def index
-    url = URI('https://api.scalablepress.com/v2/categories')
-    array = Net::HTTP.get(url)
-    @json = JSON.parse(array)
+    #url = URI('https://api.scalablepress.com/v2/categories')
+    #array = Net::HTTP.get(url)
+    #@json = JSON.parse(array)
+    
+    
+    uri = URI.parse('https://api.scalablepress.com/v2/design/5870001e3f015b8579f8945c')
+    # @products = Net::HTTP.new(produrl)
+    req = Net::HTTP::Get.new(uri)
+    req.basic_auth '', 'test_9mKVUq18F2aktJ0Sihdpkw'
+    @json = JSON.parse(req)
+    
   end
 
   def order
